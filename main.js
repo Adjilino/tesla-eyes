fdb663f39a9db0bd1acf3be8892efaf80b98a4d7
Rui Adelino <ruiadelino.monte@gmail.com>
2022-06-22 20:27:57 +0100

    Change name and icon

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function createWindow() {
         minWidth: 750,
         minHeight: 600,
         backgroundColor: "#ffffff",
-        icon: `file://${__dirname}/dist/assets/logo.png`,
+        icon: `file://${__dirname}/dist/assets/logo.icns`,
         webPreferences: {
             nodeIntegration: true,
             contextIsolation: false,
