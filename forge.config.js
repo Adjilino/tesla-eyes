ead4f3d1ed0485a9a72b225b0e901b5701869700
Rui Adelino <ruiadelino.monte@gmail.com>
2022-06-28 19:27:37 +0100

    forge config

--- forge.config.js
+++ forge.config.js
@@ -7,25 +7,8 @@ module.exports = {
     },
     makers: [
         {
-            name: "@electron-forge/maker-squirrel",
-            config: {
-                name: "Tesla Eyes",
-            },
-        },
-        {
             name: "@electron-forge/maker-zip",
-            config: {
-                name: "Tesla Eyes",
-            },
             platforms: ["darwin", "mas", "win32", "linux"],
-        },
-        {
-            name: "@electron-forge/maker-deb",
-            config: {},
-        },
-        {
-            name: "@electron-forge/maker-rpm",
-            config: {},
-        },
+        }
     ],
 };
