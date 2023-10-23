6f2b35f6a3c66cbfde3b1b7cd5fc5d5b4b6de039
Adjilino <ruiadelino.monte@gmail.com>
2023-10-23 19:11:51 +0100

    fix: remove occurrence after delete

--- src/models/occurence-files.ts
+++ src/models/occurence-files.ts
@@ -2,34 +2,40 @@ import { FileEntry } from "@tauri-apps/api/fs";
 import { OccurenceBuilder } from "../builders/occurence.builder";
 import { Config } from "./config";
 import { Occurrence } from "./occurence";
+import { uuidv4 } from "../utils";
 
 export class OccurrenceFiles {
-  files: Array<File | FileEntry> = [];
-  config?: Config = undefined;
-  thumbnail?: string | undefined = undefined;
+    protected id: string = uuidv4();
+    files: Array<File | FileEntry> = [];
+    config?: Config = undefined;
+    thumbnail?: string | undefined = undefined;
 
-  setConfig(config: Config | undefined): void {
-    this.config = config;
-  }
-  getConfig(): Config | undefined {
-    return this.config;
-  }
+    getId(): string {
+        return this.id;
+    }
 
-  setThumbnail(thumbnail: string | undefined): void {
-    this.thumbnail = thumbnail;
-  }
-  getThumbnail(): string | undefined {
-    return this.thumbnail;
-  }
+    setConfig(config: Config | undefined): void {
+        this.config = config;
+    }
+    getConfig(): Config | undefined {
+        return this.config;
+    }
 
-  setFiles(files: Array<File | FileEntry>): void {
-    this.files = files;
-  }
-  getFiles(): Array<File | FileEntry> {
-    return this.files;
-  }
+    setThumbnail(thumbnail: string | undefined): void {
+        this.thumbnail = thumbnail;
+    }
+    getThumbnail(): string | undefined {
+        return this.thumbnail;
+    }
 
-  toOccurrence(): Promise<Occurrence | undefined> {
-    return new OccurenceBuilder().addFiles(this.files).build();
-  }
+    setFiles(files: Array<File | FileEntry>): void {
+        this.files = files;
+    }
+    getFiles(): Array<File | FileEntry> {
+        return this.files;
+    }
+
+    toOccurrence(): Promise<Occurrence | undefined> {
+        return new OccurenceBuilder().addFiles(this.files).build();
+    }
 }
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,3 @@
 export * from './to-base64';
 export * from './tauri';
-
+export * from './uuid';
