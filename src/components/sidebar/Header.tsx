d0867820c799d5fd52a8c38299a737ae2f258c12
Adjilino <ruiadelino.monte@gmail.com>
2023-11-11 11:31:57 +0000

    feat: filter occurrences by reason

--- src/components/sidebar/Header.tsx
+++ src/components/sidebar/Header.tsx
@@ -1,32 +1,56 @@
-import { setIsSidebarOpen } from "../../stores";
+import { For } from "solid-js";
+import {
+    Filter,
+    currentFilter,
+    setCurrentFilter,
+    setIsSidebarOpen,
+} from "../../stores";
 import { Button } from "../../ui";
 
 export function SidebarHeader() {
-  function toggleSidebar() {
-    setIsSidebarOpen((o) => !o);
-  }
+    function toggleSidebar() {
+        console.log(Object.keys(Filter));
 
-  return (
-    <div class="flex w-full truncate">
-      <div class="flex grow items-center">
-        <Button onClick={toggleSidebar} class='dark:bg-white'>
-          <i class="mx-2 fa-solid fa-fw fa-bars" />
-        </Button>
-        <h1 class="truncate ml-2">Tesla eyes</h1>
-      </div>
+        setIsSidebarOpen((o) => !o);
+    }
 
-      <div class="flex items-center justify-items-center">
-        <a
-          class="px-2 font-semibold hover:cursor-pointer hover:underline"
-          href="https://github.com/Adjilino/tesla-eyes/issues"
-          target="_blank"
-        >
-          <i class="mr-2 fa-solid fa-fw fa-bug" />
-          <span class="truncate">Issues</span>
-        </a>
-      </div>
-    </div>
-  );
+    return (
+        <div class="flex w-full truncate">
+            <div class="flex grow items-center">
+                <Button onClick={toggleSidebar} class="dark:bg-white">
+                    <i class="mx-2 fa-solid fa-fw fa-bars" />
+                </Button>
+            </div>
+
+            <div class="flex grow items-center gap-3">
+                <For each={Object.keys(Filter)}>
+                    {(key) => (
+                        <a
+                            class={[
+                                "hover:underline hover:cursor-pointer",
+                                currentFilter() === Filter[key]
+                                    ? "font-bold"
+                                    : "",
+                            ].join(" ")}
+                            onClick={() => setCurrentFilter(Filter[key])}
+                        >
+                            {key}
+                        </a>
+                    )}
+                </For>
+            </div>
+
+            <div class="flex items-center justify-items-center">
+                <a
+                    class="px-2 font-semibold hover:cursor-pointer hover:underline"
+                    href="https://github.com/Adjilino/tesla-eyes/issues"
+                    target="_blank"
+                >
+                    <i class="mr-2 fa-solid fa-fw fa-bug" />
+                </a>
+            </div>
+        </div>
+    );
 }
 
 export default SidebarHeader;
