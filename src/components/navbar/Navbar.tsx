9cc7793e0ece6e19d6cad09ad1c4fd80a8b5ae4e
Adjilino <ruiadelino.monte@gmail.com>
2023-11-11 17:30:24 +0000

    feat: add date to title

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,21 +1,35 @@
-import { setIsSidebarOpen } from "../../stores";
+import { createMemo } from "solid-js";
+import { selectedOccurrence, setIsSidebarOpen } from "../../stores";
 import Button from "../../ui/Button";
 
 export function Navbar() {
-  const toggleSidebar = () => {
-    setIsSidebarOpen((o) => !o);
-  };
+    const toggleSidebar = () => {
+        setIsSidebarOpen((o) => !o);
+    };
 
-  return (
-    <div class="h-16 p-2 flex w-full items-center gap-2">
-      <Button onClick={toggleSidebar}>
-        <i class="mx-2 fa-solid fa-fw fa-bars"/>
-      </Button>
-      <div class="flex-grow flex">
-        <div>Tesla eyes</div>
-      </div>
-    </div>
-  );
+    const dateTitle = createMemo(() => {
+        if (selectedOccurrence() && selectedOccurrence()?.getDateTime()) {
+            return (
+                selectedOccurrence()?.getDateTime()?.toDateString() +
+                ", " +
+                selectedOccurrence()?.getDateTime()?.toLocaleTimeString()
+            );
+        }
+
+        return "";
+    });
+
+    return (
+        <div class="h-16 p-2 flex w-full items-center gap-2">
+            <Button onClick={toggleSidebar}>
+                <i class="mx-2 fa-solid fa-fw fa-bars" />
+            </Button>
+            <div class="flex">
+                <div>Tesla eyes</div>
+            </div>
+            <div class="flex-grow flex justify-center">{dateTitle()}</div>
+        </div>
+    );
 }
 
 export default Navbar;
